cb6e1a195d652dc1923feb543205fd1ca474540d
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-08-25 09:18:15 +0900

    교과과정 layout 6 교과서

--- ng-shiny/bs5-cardboard/app.R
+++ ng-shiny/bs5-cardboard/app.R
@@ -1,9 +1,15 @@
-library(shiny)
-library(bs4Dash)
-library(fresh)
+#####################################################
+#
+# bs4dash 부츠트랩 버전 5 R마크다운을 shiny에 가져와서
+# 이를 card board로 적용시키려는 시도...
+# 실패... 쭉 자동 변환만 시킴 ==> 3x3 아닌 9x1 이 됨
+#
+#####################################################
 
 library(shiny)
 library(knitr)
+library(bs4Dash)
+library(fresh)
 
 ui <- shinyUI(
   fluidPage(
